3383eca85bc5feeda16b2d5d2d1df2e82a76edd5
tanaka <lemo.nade.engineer.room@gmail.com>
2024-09-06 20:38:51 +0900

    chore: 🔧 Swift-Lint導入

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import Foundation
 import PackageDescription
 
 let package = Package(
@@ -14,17 +15,37 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-testing.git", from: "0.10.0"),
+        .package(url: "https://github.com/realm/SwiftLint.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "EventProducer"),
+            name: "EventProducer",
+            plugins: swiftLintPlugins
+        ),
         .testTarget(
             name: "EventProducerTests",
             dependencies: [
                 "EventProducer",
                 .product(name: "Testing", package: "swift-testing"),
-            ]),
+            ],
+            plugins: swiftLintPlugins
+        ),
     ]
 )
+var swiftLintPlugins: [Target.PluginUsage] {
+    guard Environment.enableSwiftLint else { return [] }
+    return [
+        .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")
+    ]
+}
+
+enum Environment {
+    static func get(_ key: String) -> String? {
+        ProcessInfo.processInfo.environment[key]
+    }
+    static var enableSwiftLint: Bool {
+        Self.get("SWIFTLINT") == "true"
+    }
+}
